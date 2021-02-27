540efecca7212ba7ef362e12895d9e31b7f7c4ca
Matt Kruse <matt@mattkruse.com>
2021-02-27 14:13:04 -0600

    Updates for 1.1

--- extension/content.js
+++ extension/content.js
@@ -82,9 +82,17 @@ function createContainer() {
   dom_container = $el('div',{id:"talk-time-container"});
   dom_container.innerHTML = `
     <div class="talk-time-top" title="Click to collapse/expand">
+      <svg class="talk-time-options-gear" title="Talk Time Options" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z"/></svg>
       <div class="talk-time-title">Talk Time</div>
     </div>
+    <div class="talk-time-options-content">
+      <h3>Options</h3>
+      <p style="font-style:italic;">No options available at this time</p>
+      <button class="talk-time-options-close">Close</button>
+    </div>
     <div class="talk-time-header">
+      <div class="talk-time-show-groups">Show Groups</div>
+      <div class="talk-time-hide-groups">Hide Groups</div>
       <div class="talk-time-summary">Total Talk Time: <span id="talk-time-summary-total"></span></div>
     </div>
     <div class="talk-time-body">
@@ -96,9 +104,17 @@ function createContainer() {
   document.body.appendChild(dom_container);
   dom_table = dom_container.querySelector('table');
   dom_total = dom_container.querySelector('#talk-time-summary-total');
-  dom_container.querySelector('.talk-time-top').addEventListener('click',function() {
-    dom_container.classList.toggle("collapsed");
-  })
+  let onclick=function(selector,f) {
+    dom_container.querySelector(selector).addEventListener('click',f);
+  };
+  onclick('.talk-time-top',()=>{ dom_container.classList.toggle("collapsed"); });
+  onclick('.talk-time-show-groups',()=>{ dom_container.classList.add('show_groups'); });
+  onclick('.talk-time-hide-groups',()=>{ dom_container.classList.remove('show_groups'); });
+  onclick('.talk-time-options-gear',(e)=>{
+    e.stopPropagation();
+    dom_container.classList.add('talk-time-options');
+  });
+  onclick('.talk-time-options-close',()=>{ dom_container.classList.remove("talk-time-options"); });
 }
 
 // Create the group rendering table
